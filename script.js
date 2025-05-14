1db5039610b530528ef2efc29783e91c3f9dda91
sharif <uddin15-4475@diu.edu.bd.com>
2025-05-14 22:13:17 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for future functionality (e.g., show popup)
+document.querySelector('.quote-button').addEventListener('click', () => {
+    alert('Thank you for your interest! A quote request popup can go here.');
+  });
+  
